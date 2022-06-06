a9542e9fccc2c509d1ed83e372a92daff604f7a2
LeleGuidi <emanuele.guidi98@outlook.it>
2022-06-06 16:39:55 +0200

    Inserite le card nella seconda sezione

--- src/components/blocks/BaseMain.vue
+++ src/components/blocks/BaseMain.vue
@@ -1,21 +1,27 @@
 <template>
     <main class="main">
         <SectionHero/>
+        <SectionCards/>
     </main>
 </template>
 
 <script>
 import SectionHero from '../sections/SectionHero.vue'
+import SectionCards from '../sections/SectionCards.vue'
 export default {
-  components: { SectionHero },
+    name: 'BaseMain',
+    components: { 
+        SectionHero, 
+        SectionCards 
+    },
     
 }
 </script>
 
 <style scoped lang="scss">
 @import '../../assets/style/mixins.scss';
-    .main {
-
-    }
+.main {
+    background-color: #1c1c1c;
+}
 </style>
 
---
+++ src/components/commons/CommonCard.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="card">
+        <img :src="thumb" :alt="series">
+        <h4>{{series}}</h4>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'CommonCard',
+    props: {
+        thumb: String,
+        series: String,
+    },
+}
+</script>
+
+<style lang="scss" scoped>
+.card {
+    text-align: left;
+
+    img {
+        object-fit: contain;
+        object-position: top;
+        width: 100%;
+    }
+    h4 {
+        margin-top: 0.6rem;
+        font-weight: 500;
+        font-size: var(--font-sm);
+    }
+}
+</style>
